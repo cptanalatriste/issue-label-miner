4ae2a2eba646bf3f71a30178d3265e9be33f2e55
Carlos G. Gavidia <cgavidiac@gmail.com>
2019-08-15 16:42:00 +0100

    All hour manual validation in a single place

--- valheuristics.py
+++ valheuristics.py
@@ -3,27 +3,17 @@ import pandas as pd
 
 import labelanalysis
 
-ASSESMENT_COLUMN = "Carlos: Using labels as priority?"
-REPOSITORY_URL_COLUM = "url"
+ASSESMENT_COLUMN = "manual_assessment"
 
 
-def get_validation_report(assessment_csv="assessed_sample.csv", assessment_column=ASSESMENT_COLUMN):
-    assessed_dataframe = pd.read_csv(assessment_csv)
+def get_validation_report(assessed_dataframe):
     heuristic_dataframe_all = pd.read_csv(labelanalysis.CONSOLIDATED_CSV_FILE)
 
-    assessed_dataframe[assessment_column] = assessed_dataframe[assessment_column].map(
-        {"FALSE": False,
-         "TRUE": True,
-         "NONE": None})
-
-    filter = assessed_dataframe[assessment_column].notnull()
-    assessed_dataframe = assessed_dataframe[filter]
-
-    consolidated_dataframe = pd.merge(assessed_dataframe, heuristic_dataframe_all, left_on=REPOSITORY_URL_COLUM,
-                                      right_on=labelanalysis.REPOSITORY_URL_COLUMN)
+    consolidated_dataframe = pd.merge(assessed_dataframe, heuristic_dataframe_all,
+                                      on=labelanalysis.REPOSITORY_URL_COLUMN)
 
     heuristic_results = consolidated_dataframe[labelanalysis.USING_PRIORITIES_COLUMN].astype(int)
-    manual_validation = consolidated_dataframe[assessment_column].astype(int)
+    manual_validation = consolidated_dataframe[ASSESMENT_COLUMN].astype(int)
 
     target_names = ["not using priorities", "using priorities"]
 
@@ -35,14 +25,35 @@ def get_validation_report(assessment_csv="assessed_sample.csv", assessment_colum
                                 target_names=target_names))
 
 
-def consolidate_assesment_files() :
-
-    first_dataframe =  pd.read_csv("assessed_sample.csv")
+def consolidate_assesment_files():
+    first_dataframe = pd.read_csv("assessed_sample.csv")
     first_dataframe = first_dataframe[["url", "Carlos: Using labels as priority?"]]
+    first_dataframe = first_dataframe.rename(
+        columns={"Carlos: Using labels as priority?": ASSESMENT_COLUMN,
+                 "url": labelanalysis.REPOSITORY_URL_COLUMN})
 
-    print(first_dataframe.head())
+    second_dataframe = pd.read_csv("assessed_sample_custom.csv")
+    second_dataframe = second_dataframe[[labelanalysis.REPOSITORY_URL_COLUMN, "Carlos: Using Priorities?"]]
+    second_dataframe = second_dataframe.rename(
+        columns={"Carlos: Using Priorities?": ASSESMENT_COLUMN})
+
+    consolidated_dataframe = pd.concat([first_dataframe, second_dataframe]).drop_duplicates().reset_index(drop=True)
+
+    consolidated_dataframe[ASSESMENT_COLUMN] = consolidated_dataframe[
+        ASSESMENT_COLUMN].map(
+        {"FALSE": False,
+         "TRUE": True,
+         "NONE": None})
+
+    filter = consolidated_dataframe[ASSESMENT_COLUMN].notnull()
+    consolidated_dataframe = consolidated_dataframe[filter]
+
+    return consolidated_dataframe
+
 
 if __name__ == "__main__":
-    # get_validation_report()
+    consolidated_dataframe = consolidate_assesment_files()
 
-    consolidate_assesment_files()
+    print(consolidated_dataframe.describe())
+
+    get_validation_report(consolidated_dataframe)
